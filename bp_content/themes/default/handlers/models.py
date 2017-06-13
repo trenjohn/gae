c0293b75ef53bcbf11d24f26a03c8171519424b4
Trent Johnson <trent.matt.j@gmail.com>
2017-06-13 17:43:16 -0600

    JSON user sign-up for game.

--- bp_content/themes/default/handlers/models.py
+++ bp_content/themes/default/handlers/models.py
@@ -6,7 +6,7 @@ class Game(ndb.Model):
     #user = ndb.ReferenceProperty(Account)
     numberPlayers = ndb.IntegerProperty()
     entryFee = ndb.FloatProperty()
-    usersSignedUp = ndb.JSONProperty()
+    usersSignedUp = ndb.JsonProperty()
     #PrizeStructure (Model w/ variety of types)
     #StartTime
     #Duration
