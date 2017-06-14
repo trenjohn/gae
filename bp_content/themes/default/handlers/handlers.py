096d656584c8ea55325f33405d596c79b6eeb32d
Trent Johnson <trent.matt.j@gmail.com>
2017-06-13 18:42:28 -0600

    JSON user sign-up for game.

--- bp_content/themes/default/handlers/handlers.py
+++ bp_content/themes/default/handlers/handlers.py
@@ -313,7 +313,14 @@ class GamePageHandler(BaseHandler):
         return self.render_template('game.html', **params)
 
     def post(self, url):
-        try:
+
+        url = self.request.url
+        gameID = os.path.basename(os.path.normpath(url))
+        gameID = int(gameID)
+        game = Game()
+        game = game.get_by_id(gameID)
+
+        if not game.usersSignedUpCount == game.numberPlayers:
 
             url = self.request.url
             gameID = os.path.basename(os.path.normpath(url))
@@ -322,7 +329,7 @@ class GamePageHandler(BaseHandler):
             game = game.get_by_id(gameID)
 
             game.usersSignedUpCount = game.usersSignedUpCount + 1
-            
+
             if game.usersSignedUp:
                 game.usersSignedUp.append({'user': self.user_id})
             else:
@@ -332,5 +339,6 @@ class GamePageHandler(BaseHandler):
 
             return self.redirect_to(url)
 
-        except:
+        else:
+            
             return self.redirect_to('lobby')
